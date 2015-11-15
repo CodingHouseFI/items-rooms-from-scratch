b84d55a73aa914e0b8654d357b9154f15560e901
Cade Nichols <cade@cadenichols.com>
2015-11-14 16:02:10 -0800

    init and item model

---
+++ models/item.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Item;
+
+var itemSchema = Schema({
+  name: { type: String, required: false },
+  value: { type: Number, required: false },
+  description: { type: String },
+  createdAt: { type: Date, default: new Date() }
+});
+
+Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
